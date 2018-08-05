375641a9e59c8bc078a5cb773604e9555d40bbc6
Rati <rjp76@njit.edu>
2018-08-05 19:16:02 -0400

    added list of question on homepage and a pager (did not want to commit for some reason :/)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
                                             <div class="card-header">
                                                 <small class="text-muted">
                                                     Updated: {{ $question->created_at->diffForHumans() }}
-                                                    Answers: {{ $question->answers()->count() }} 
+                                                    Answers: {{ $question->answers()->count() }}
 
                                                 </small>
                                             </div>
